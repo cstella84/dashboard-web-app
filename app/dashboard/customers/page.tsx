4522f4a0d8b95541369f897687a34ebea75c1166
cstellahc <christopher.stella@hashicorp.com>
2025-02-11 23:02:57 -0700

    added customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
+import React from 'react';
 import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
 
 export const metadata: Metadata = {
   title: 'Invoices',
 };
 
-export default function Page() {
-    return <p>Customers Page</p>
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <div className="w-full">
+      <CustomersTable customers={customers} />
+    </div>
+    )
 }
